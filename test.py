98024a3c6f91d0e5d8959624d1f2556558ec1a4c
nrshka <retributioniswar@gmail.com>
2019-08-27 16:34:11 +0700

    randomized test

--- test.py
+++ test.py
@@ -2,20 +2,30 @@ from Graph import Graph, Node
 from web import web_gen
 from PIL import Image
 
-
+import random as rand
 
 
 
 
 
 if __name__ == "__main__":
+  count_nodes = 10
   flower_img = Image.open('flower.jpg')
-  g = Graph([Node(flower_img, 50, 'title1'), Node(flower_img, 20, 'title2'),
-            Node(flower_img, 10, 'title3'), Node(flower_img, 90, 'title4')])
+  # g = Graph([Node(flower_img, 50, 'title1'), Node(flower_img, 20, 'title2'),
+  #           Node(flower_img, 10, 'title3'), Node(flower_img, 90, 'title4'),
+  #           Node(flower_img, 10, 'title5'), Node(flower_img, 10, 'title6'),
+  #           Node(flower_img, 10, 'title7'), Node(flower_img, 10, 'title8'),
+  #           Node(flower_img, 10, 'title9'), Node(flower_img, 10, 'title10')])
+  g = Graph([Node(flower_img, 10, 'title' + str(i + 1)) for i in range(count_nodes)])
   g.add_edge(0, 'title2')
   g.add_edge(1, 2)
   g.add_edge(2, 0)
   g.add_edge(3, 0)
+  for i in range(1000):
+    for j in range(rand.randint(0, 2*count_nodes))
+      a = rand.randint(0, count_nodes)
+      b = rand.randint(0, count_nodes)
+      g.add_edge(a, b)
   
   print(g.edges)
   web_gen.get_page(g)
